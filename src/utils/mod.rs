2c59065a9a0db9062043c0eec767ccc22c4548fa
BL-CZY <jiguangllsw@gmail.com>
2024-05-13 20:34:16 +0200

    print the entire structure

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -7,10 +8,10 @@ use crate::view::structs::{ColorType, NumberType, PivotType, PointType, TextAlig
 
 pub fn print_number_type(target: &NumberType) {
     match target {
-        NumberType::Pixel(val) => print!("{}px", val),
-        NumberType::Em(val) => print!("{}em", val),
-        NumberType::Percent(val) => print!("{}%", val),
-        NumberType::Number(val) => print!("{}", val),
+        NumberType::Pixel(val) => print!("{}{}", val.to_string().yellow(), "px".yellow()),
+        NumberType::Em(val) => print!("{}{}", val.to_string().yellow(), "em".yellow()),
+        NumberType::Percent(val) => print!("{}{}", val.to_string().yellow(), "%".yellow()),
+        NumberType::Number(val) => print!("{}", val.to_string().yellow()),
     }
 }
 
@@ -19,81 +20,86 @@ pub fn print_point_type(target: &PointType) {
     print_number_type(&target.x);
     print!(", ");
     print_number_type(&target.y);
-    print!(")")
+    print!(")");
 }
 
 pub fn print_pivot_type(target: &PivotType) {
     match target {
-        PivotType::BottomCenter => print!("bottom center"),
-        PivotType::BottomLeft => print!("bottom left"),
-        PivotType::BottomRight => print!("bottom right"),
-        PivotType::LeftCenter => print!("left center"),
-        PivotType::RightCenter => print!("right center"),
-        PivotType::TopCenter => print!("top center"),
-        PivotType::TopLeft => print!("top left"),
-        PivotType::TopRight => print!("top right"),
-        PivotType::Center => print!("center"),
+        PivotType::BottomCenter => print!("{}", "bottom center".yellow()),
+        PivotType::BottomLeft => print!("{}", "bottom left".yellow()),
+        PivotType::BottomRight => print!("{}", "bottom right".yellow()),
+        PivotType::LeftCenter => print!("{}", "left center".yellow()),
+        PivotType::RightCenter => print!("{}", "right center".yellow()),
+        PivotType::TopCenter => print!("{}", "top center".yellow()),
+        PivotType::TopLeft => print!("{}", "top left".yellow()),
+        PivotType::TopRight => print!("{}", "top right".yellow()),
+        PivotType::Center => print!("{}", "center".yellow()),
     }
 }
 pub fn print_color_type(target: &ColorType) {
-    print!("({}, {}, {}, {})", target.r, target.g, target.b, target.a);
+    print!(
+        "({}, {}, {}, {})",
+        target.r.to_string().yellow(),
+        target.g.to_string().yellow(),
+        target.b.to_string().yellow(),
+        target.a.to_string().yellow()
+    );
 }
 
 pub fn print_text_align(target: &TextAlignType) {
     match target {
-        TextAlignType::Left => print!("left"),
-        TextAlignType::Right => print!("right"),
+        TextAlignType::Left => print!("{}", "left".yellow()),
+        TextAlignType::Right => print!("{}", "right".yellow()),
     }
 }
 
 pub fn print_element(element: &Element, depth: u32) {
-    for _ in 0..(depth * 5) {
-        print!(" ");
+    for _ in 0..depth {
+        print!("│   ");
     }
 
-    print!("├───");
+    print!("├── ");
 
-    print!(" ");
-
-    print!("element type: ");
+    print!("{}", "Element type: ".blue());
     //print the element type
     match element.element_type {
-        ElementType::Canvas => println!("canvas"),
-        ElementType::Label => println!("label"),
-        ElementType::Panel => println!("panel"),
+        ElementType::Canvas => println!("{}", "canvas".green()),
+        ElementType::Label => println!("{}", "label".green()),
+        ElementType::Panel => println!("{}", "panel".green()),
     }
 
     for property in &element.properties {
-        for _ in 0..(depth * 3 + 5) {
-            print!(" ");
+        for _ in 0..depth + 1 {
+            print!("│   ");
         }
-        print!("│     ");
 
-        print!("--{}: ", property.0);
+        print!("│ {}{}: ", "--".cyan(), property.0.as_str().cyan());
 
         match property.1 {
             Property::Width(val) => print_number_type(val),
             Property::Height(val) => print_number_type(val),
             Property::Position(val) => print_point_type(val),
-            Property::PosRelToParent(val) => print!("{}", val),
+            Property::PosRelToParent(val) => print!("{}", val.to_string().as_str().yellow()),
             Property::Pivot(val) => print_pivot_type(val),
             Property::BackgroundColor(val) => print_color_type(val),
             Property::TextColor(val) => print_color_type(val),
-            Property::Text(val) => print!("{}", val),
-            Property::Font(val) => print!("{}", val),
+            Property::Text(val) => print!("{}", val.to_string().as_str().yellow()),
+            Property::Font(val) => print!("{}", val.to_string().as_str().yellow()),
             Property::TextAlign(val) => print_text_align(val),
         }
 
         println!("");
     }
-
+    for _ in 0..depth + 2 {
+        print!("│   ");
+    }
     println!("");
 }
 
 //recursively print the value in a tree
 pub fn print_canvas_tree(node: Rc<RefCell<CanvasNode>>, current_depth: u32) {
+    print_element(&node.borrow().value, current_depth);
     for sub_node in &node.borrow().children {
-        print_element(&sub_node.borrow().value, current_depth);
         print_canvas_tree(Rc::clone(sub_node), current_depth + 1);
     }
 }
